f7a59a52d79ae6771b2a23cd82b5764046e3a19c
kyle22112 <159410248+kyle22112@users.noreply.github.com>
2024-04-15 16:45:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,28 +6,11 @@ function addTask() {
   }
   var ul = document.getElementById("taskList");
   var li = document.createElement("li");
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.addEventListener("change", function() {
-    if (this.checked) {
-      li.classList.add("completed");
-      console.log("Checkbox checked!");
-      // Trigger confetti animation when task is completed
-      confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 }
-      });
-    } else {
-      li.classList.remove("completed");
-    }
-  });
   var removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.addEventListener("click", function() {
     ul.removeChild(li);
-  });
-  li.appendChild(checkbox);
+  })
   li.appendChild(document.createTextNode(input));
   li.appendChild(removeButton);
   ul.appendChild(li);
