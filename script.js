9f5803a646dc707cb9d19d21433d011db477673d
kyle22112 <159410248+kyle22112@users.noreply.github.com>
2024-04-15 16:57:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ function addTask() {
   }
   var ul = document.getElementById("taskList");
   var li = document.createElement("li");
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", function() {
+    if (this.checked) {
+      li.classList.add("completed");
+    } else {
+      li.classList.remove("completed");
+    }
+  });
   var removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.addEventListener("click", function() {
     ul.removeChild(li);
-  })
+  });
+  li.appendChild(checkbox);
   li.appendChild(document.createTextNode(input));
   li.appendChild(removeButton);
   ul.appendChild(li);
